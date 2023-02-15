cbcb8bdf846be09c69a19fdda5b7fce860cb36ee
kouraichiahmed2 <kouraichiahmed2@gmail.com>
2023-02-15 20:52:47 +0100

    first commit

---
+++ src/main/java/com/example/demo/HibernateCompositePrimaryKeyExamplewithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCompositePrimaryKeyExamplewithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCompositePrimaryKeyExamplewithSpringBootApplication.class, args);
+	}
+
+}
